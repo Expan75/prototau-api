de0188542fe0fdabc74b10720d9009a8eb6f73c8
stigsvensson <erik.hakansson96@gmail.com>
2019-10-18 20:40:49 +0100

    datapoints get/post working

--- app.py
+++ app.py
@@ -1,19 +1,46 @@
-import os
-from flask import Flask
+import os, json
+from time import sleep
+from functools import reduce
+from flask import Flask, jsonify, request
 
 """ Init & Settings """ 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
+# example data 
+collection = [{
+    'id': 1,
+    'title': 'Something with a title',
+    'content': 'LoremIpsum',
+    'type': False,
+    },
+    {
+    'id': 2, 
+    'title': 'Something different title',
+    'content': 'LoremIpsum2',
+    'type': True,
+    },
+]
+
 @app.route('/')
 def hello():
-    print(os.environ['APP_SETTINGS'])
-    return "Hello World!"
+    return "Welcome to the Prototau API. Please hold while you're redirected to the offical documentation..."
 
 
-@app.route('/<name>')
+@app.route('/auth/<name>')
 def hello_name(name):
-    return "Hello {}!".format(name)
+    return "Hello {}; you are!".format(name)
+
+@app.route('/datapoints', methods=['GET','POST'])
+def DataPointsHandler():
+    if request.method == 'POST':
+        data = json.loads(request.data)
+        # Atomic data entry
+        for obj in data:
+            collection.append(obj)
+
+    # Customise return object to include a status message (should confirm CRUD operation)
+    return jsonify(collection)
 
 if __name__ == '__main__':
     app.run()
